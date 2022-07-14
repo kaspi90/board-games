d605c98a90a4647123cfd3c101cf17e3bd57dfae
Heike Kasper <heike.kasper@kadelo.de>
2022-07-14 14:20:51 +0200

    created component footer and styled the footer

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,9 +1,12 @@
+import Footer from "./Footer";
 import Header from "./Header";
 import "./Homepage.css";
 function Homepage() {
   return (
     <div>
       <Header />
+      <div className="main-section"></div>
+      <Footer />
     </div>
   );
 }
