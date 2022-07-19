13731e7c34e52aa151b940d642053e89f4bfc245
Heike Kasper <heike.kasper@kadelo.de>
2022-07-19 12:44:47 +0200

    added function for api and styled the boardgame container

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,11 +1,31 @@
+import React from "react";
+import Games from "./Boardgame";
 import Footer from "./Footer";
 import Header from "./Header";
 import "./Homepage.css";
+import axios from "axios";
+
+const convert = require("xml-js");
+
 function Homepage() {
+  const [bgames, setGames] = React.useState([]);
+  axios
+    .get("https://boardgamegeek.com/xmlapi2/hot?boardgame")
+    .then((response) => {
+      const datagames = JSON.parse(
+        convert.xml2json(response.data, { compact: true })
+      );
+      setGames(datagames.items.item);
+      console.log(bgames);
+    });
   return (
     <div>
       <Header />
-      <div className="main-section"></div>
+      <div className="main-section">
+        {bgames.map((gamesObject) => (
+          <Games boardgames={gamesObject} />
+        ))}
+      </div>
       <Footer />
     </div>
   );
